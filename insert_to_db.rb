3efdd6860dabf002631cbba3dcec72c7b6e8c550
senthil nayagam <senthil.nayagam@gmail.com>
2020-07-08 00:44:30 +0530

    wrote a simple ruby parser for the death case pattern, need to find remaining fields and insert all into database

--- insert_to_db.rb
+++ insert_to_db.rb
@@ -1,5 +1,6 @@
 require 'pg'
-
+require 'pp'
+=begin
 connection = PG.connect :user => 'postgres', :password => 'root', :dbname => 'tamilnadu_case'
 
 #connection.exec "CREATE DATABASE tamilnadu_case"
@@ -11,16 +12,89 @@ connection.exec "CREATE TABLE Cases(Id INTEGER PRIMARY KEY, Summary TEXT)"
 puts "Table created"
 
 puts "Creating data......"
+=end
 
-(1..1571).each do |i|
+def parse_data(file_string)
+fs = file_string
+
+# join string as single line , replace new line with space
+fs =  fs.gsub("\n", " ")
+
+# replace multiple spaces with single space
+fs =  fs.gsub(/\s+/, ' ')
+
+# find gender
+if fs.downcase.include? 'female'
+gender = 'female'
+else
+gender = 'male'
+end
+
+#puts gender
+# find age
+
+
+numbers = fs.scan(/\d+/)
+#fs =  fs.gsub("years", "year")
+#m = fs.match /\w+ (\d+)/
+#pp numbers
+case_number = numbers[0]
+age = numbers[1]
+
+
+# find district
+
+
+words = fs.scan(/\w+/)
+
+#pp words
+from_index = words.index('from')
+district = words[from_index + 1] unless from_index.nil?
+	
+
+# find cause of death
+
+cause = fs.split("due to")
+
+death_cause = cause[1] if cause.length ==2
+
+puts "raw_content:" + fs
+puts "case_number:" + case_number
+puts "age:" + age.to_s
+puts "gender:" + gender.to_s
+puts "district:" + district.to_s
+puts "death_cause:" + death_cause.to_s
+puts
+
+
+# return as hash and use it in the sql insert statement appropriately
+
+
+
+
+
+
+
+end
+
+
+(1..1636).each do |i|  # 1.. 1636
 	
 	f = File.open("./cases/case_"+i.to_s.rjust(4, "0")+".txt")
+
+
+ parse_data(f.read)
+
+=begin	
 	connection.exec "INSERT INTO Cases (Id, Summary) VALUES( 
 						#{i}, 
 						'#{f.read}'
 					)"
+=end					
 end
 
+
+=begin
 puts "Data inserted successfully"
 
 res = connection.exec "SELECT * FROM cases LIMIT 5"
@@ -28,7 +102,7 @@ res = connection.exec "SELECT * FROM cases LIMIT 5"
 puts "printing some sample data"
 
 puts res.map { |row| puts row } 
-
+=end
 
 
 
--- ruby_script_readme.txt
+++ ruby_script_readme.txt
@@ -3,3 +3,19 @@ Step1: Create Database tamilnadu_case
 Step2: Then change you database details
 
 Step3: run "ruby insert_to_db.rb"
+
+
+
+disabled sql insert for some time to do parsing first 
+
+
+ruby insert_to_db.rb > output.txt
+
+
+sample output
+Death Case No.1636: A 69 years old female from Kanyakumari, Brought Dead on 28.06.2020 at 08.00 AM in Kanyakumari Govt Medical College & Hospital, Asaripallam. The patient referral from Govt Hospital, Thuckalay. COVID test sample taken on 28.06.2020. Outcome of result is Positive. The patient died on 28.06.2020 at 08.00AM due to COVID Positive. 
+case_number:1635
+age:83
+gender:female
+district:Chennai
+death_cause: Viral Pneumonia / COVID Infection / COVID Positive. 
