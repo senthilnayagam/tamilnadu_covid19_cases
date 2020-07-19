d10d63bdca8c54a5a18a4c2cc9058eaa9e124632
senthil nayagam <senthil.nayagam@gmail.com>
2020-07-20 01:18:52 +0530

    more data corrections

--- scripts/date_report.rb
+++ scripts/date_report.rb
@@ -15,9 +15,27 @@ begin
     # columns in table
     # RawContent, CaseNumber, Age, Gender, District, DeathCause, admitted_on ,died_on , sample_taken_on , result_on 
 
+
+
     ary.each do |row|
-        printf "%s, %s,  %s,  %s,  %s,  %s,  %s \n", row['CaseNumber'], row['District'], row['Age'], row['admitted_on'],row['died_on'],row['sample_taken_on'],row['result_on']
-        puts Date.strptime(row['admitted_on'], '%d-%m-%Y') if row['admitted_on'] != ''
+    	admitted_on,died_on,sample_taken_on,result_on='','','',''
+        #printf "%s, %s,  %s,  %s,  %s,  %s,  %s \n", row['CaseNumber'], row['District'], row['Age'], row['admitted_on'],row['died_on'],row['sample_taken_on'],row['result_on']
+        
+        begin
+
+        admitted_on =Date.strptime(row['admitted_on'], '%d-%m-%Y') if row['admitted_on'] != ''
+        died_on  = Date.strptime(row['died_on'], '%d-%m-%Y') if row['died_on'] != ''
+        sample_taken_on =  Date.strptime(row['sample_taken_on'], '%d-%m-%Y') if row['sample_taken_on'] != ''
+        result_on = Date.strptime(row['result_on'], '%d-%m-%Y') if row['result_on'] != ''
+        puts "case: " + row['CaseNumber'].to_s + " # admission to death: " + (died_on-admitted_on).to_i.to_s if died_on !='' && admitted_on !=''
+
+    rescue ArgumentError => ee
+    	puts ee
+    	printf "%s, %s,  %s,  %s,  %s,  %s,  %s \n", row['CaseNumber'], row['District'], row['Age'], row['admitted_on'],row['died_on'],row['sample_taken_on'],row['result_on']
+    end
+
+
+
     end
 
 
